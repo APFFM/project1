df28c0ca1adfd9bd86152f683565d9d036ac2bee
APFFM <83350010+APFFM@users.noreply.github.com>
2023-11-15 23:44:08 +0100

    Create styles.cs

---
+++ styles.cs
@@ -0,0 +1,25 @@
+body {
+  font-family: Arial, sans-serif;
+  background-color: #f4f4f4;
+  margin: 0;
+  padding: 0;
+}
+
+#main-container {
+  width: 80%;
+  margin: 0 auto;
+  padding: 20px;
+  background-color: #fff;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+#ticker-input {
+  padding: 10px;
+  margin-right: 10px;
+  width: 300px;
+}
+
+button {
+  padding: 10px 20px;
+  cursor: pointer;
+}
